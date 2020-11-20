04c105a3c0caefaff71efeb9a1037369ef65b405
fwcd <fwcdmail@gmail.com>
2020-11-21 00:51:57 +0100

    Implement basic peano numbers

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Hello world!");
+type Zero = { zero: true };
+type Succ<N> = { succ: N; }
+
+type Add<N, M> = N extends Succ<infer I>
+    ? Succ<Add<I, M>>
+    : M;
+
+function forceEqual<T1, T2 extends T1>() {}
+
+// Only type-checks if 1 + 1 = 2
+forceEqual<Add<Succ<Zero>, Succ<Zero>>, Succ<Succ<Zero>>>();
